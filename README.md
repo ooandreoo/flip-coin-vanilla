e6604ef14a71ad1f89c12a95698d97de0951dc80
ooandreoo <andre_av_5@outlook.com>
2025-03-31 11:54:16 -0300

    sample web site for portfolio added

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # flip-coin-vanilla
 A flip coin web site that flips a coin in case you believe in luck!
+
+## Stack
+
+HTML + CSS + JavaScript
+
+## How to see this thing working?
+
+1. Install node and npm
+2. Install http-server: npm install --global http-server
+3. Run http-server on the project directory
+4. Open a web browser and go to 'localhost:8080'
+
+## Why is it so lame?
+
+It is still a v1.0, it will get better :D
---
+++ index.js
@@ -0,0 +1,45 @@
+document.getElementById('flip-button').addEventListener('click', async function(event) {
+  let coin = document.getElementById('coin-svg');
+  await flipCoinRandomly(coin);
+});
+
+async function flipCoinRandomly(coin) {
+  await removePreviousAnimationClass(coin);
+  let i = getRandomInt(2);
+  var flipClass;
+  if(i>0)
+    flipClass = 'flippin-face';
+  else
+    flipClass = 'flippin-shield';
+  coin.classList.add(flipClass); 
+  await sleep(7000);
+  enableButton();
+}
+
+async function removePreviousAnimationClass(coin) {
+  disableButton();
+  if(coin.classList.contains('flippin-face'))
+    coin.classList.remove('flippin-face');
+  else if(coin.classList.contains('flippin-shield')){
+    coin.classList.add('flip-complete');
+    await sleep(1500);
+    coin.classList.remove('flip-complete');
+    coin.classList.remove('flippin-shield');
+  }
+}
+
+function disableButton() {
+  document.getElementById('flip-button').disabled = true;
+}
+
+function enableButton() {
+  document.getElementById('flip-button').disabled = false;
+}
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms))
+} 
